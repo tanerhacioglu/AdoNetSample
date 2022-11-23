c675496ee99b2576e2e889b629603e84bc200e2f
Taner Hacıoğlu <30664083+tanerhacioglu@users.noreply.github.com>
2022-11-23 11:05:24 +0300

    .
    .

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using AdoNETSampleBL;
+using AdoNETSampleDAL.DataSet;
+using PgSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AdoNETSampleBL.AdoNETSampleBL bL = new AdoNETSampleBL.AdoNETSampleBL();
+            PgSqlFilter filter = null;
+            PgSqlOrder order = null;
+
+            dsSampleCategory ds = bL.SelectSampleCategory(filter, order);
+
+            foreach (var row in ds.SampleCategory)
+            {
+                Console.WriteLine(row.ID.ToString() + " " + row.Name.ToString());
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
